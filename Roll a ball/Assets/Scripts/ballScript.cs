71177736c894e0b5ad3fd2272f81ffc4f114f01f
parz1 <1062134330@qq.com>
2019-05-16 01:25:35 +0800

    Form a unity-projects repo.

---
+++ Roll a ball/Assets/Scripts/ballScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballScript : MonoBehaviour {
+
+    private Rigidbody rd;
+
+    public int force = 5;
+
+	// Use this for initialization
+	void Start () {
+        rd = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rd.AddForce(new Vector3(h, 0, v)*force);
+	}
+}
